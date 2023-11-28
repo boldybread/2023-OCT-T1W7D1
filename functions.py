6d7655162a5eca808d07ebe14805fea138958f3e
boldybread <bradarchbold@gmail.com>
2023-11-28 20:54:14 +1100

    initial commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+# Functions
+# start with 'def' keyword
+# then has the name of the function
+# list of arguments
+# the lines of code that create the function
+    # print("What is your name?")
+    # name = input("Enter your name: ")
+    # print("Hi " + name)
+
+def greet(fname, lname):
+    print(f"Hello {fname} {lname}")
+    print("How are you?")
+greet("Brad", "Archbold")
+
